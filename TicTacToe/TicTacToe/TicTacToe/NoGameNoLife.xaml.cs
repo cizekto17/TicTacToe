f0232286a767cc5aba1d727ba652a28cf1c05ec4
cizekto17 <71686181+cizekto17@users.noreply.github.com>
2020-11-21 18:46:10 +0100

    vytvoření všech stránek

---
+++ TicTacToe/TicTacToe/TicTacToe/NoGameNoLife.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicTacToe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoGameNoLife : ContentPage
+    {
+        public NoGameNoLife()
+        {
+            InitializeComponent();
+        }
+    }
+}
