693bb901bba55635139fccade3b995d2f2c1cc46
cizekto17 <71686181+cizekto17@users.noreply.github.com>
2021-01-04 20:32:20 +0100

    Update NoGameNoLife2.xaml.cs

--- TicTacToe/TicTacToe/TicTacToe/NoGameNoLife2.xaml.cs
+++ TicTacToe/TicTacToe/TicTacToe/NoGameNoLife2.xaml.cs
@@ -16,6 +16,35 @@ namespace TicTacToe
         {
             InitializeComponent();
         }
+        public bool Player1 = true;
+        public bool Player2 = false;
+        public void Button_Clicked(object sender, EventArgs args)
+        {
+            ImageButton btn = (ImageButton)sender;
+
+
+            //btn.Source = ImageSource.FromFile("Images/x_player2.png");
+            //btn.Source = ImageSource.FromFile("{local:ImageResource TicTacToe.Images.o_player.png}");
+
+            if (Player1)
+            {
+                btn.Source = ImageSource.FromResource("TicTacToe.Images.x_player2.png");
+                Console.WriteLine(btn.Source);
+                btn.IsEnabled = false;
+                Player1 = false;
+                Player2 = true;
+            }
+            else if (Player2)
+            {
+                btn.Source = ImageSource.FromResource("TicTacToe.Images.o_player.png");
+                Console.WriteLine(btn.Source);
+                btn.IsEnabled = false;
+                Player1 = true;
+                Player2 = false;
+            }
+
+
+        }
         async public void Surrender(object sender, EventArgs args)
         {
             await Navigation.PushAsync(new EndOfSuffering { });
